261670164ae1aa65572b13db3df30d2279f53e91
DamianoLandi <damianolandi01@gmail.com>
2021-09-10 16:42:34 +0200

    Raccolta dati da API

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,50 @@
 <template>
-    <h2>hello</h2>
+    <div>
+        <input v-model="searchTerm" type="text">
+        <button v-on:click="getData">Cerca</button>
+    </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
-    name: "Header"
+    name: "Header",
+    data(){
+        return{
+            searchTerm: "",
+            key: "api_key=6bdd58982188b8c812708db73b34338e",
+            lang: "&language=it_IT",
+            moviesList: null,
+            tvList: null,
+        }
+    },
+
+    computed: {
+        query(){
+            return `&query=${this.searchTerm}` 
+        }
+    },
+
+    methods: {
+        getData(){
+            axios
+            .get(`https://api.themoviedb.org/3/search/movie?${this.key}${this.lang}${this.query}`)
+            .then((res) => {
+                const response = res.data.results;
+                this.moviesList = response;
+            });
+
+            axios
+            .get(`https://api.themoviedb.org/3/search/tv?${this.key}${this.lang}${this.query}`)
+            .then((res) => {
+                const response = res.data.results;
+                this.tvList = response;
+            });
+
+            this.searchTerm = "";
+        }
+    }
 }
 </script>
 
