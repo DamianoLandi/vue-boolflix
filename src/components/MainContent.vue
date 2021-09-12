74864a19e2ef029856285d819388f3a2e0ff2666
DamianoLandi <damianolandi01@gmail.com>
2021-09-13 01:12:53 +0200

    Milestone 2

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -7,7 +7,7 @@
                     <p v-else>Titolo: {{movie.name}}</p>
                     <p v-if="movie.title">Titolo Originale: {{movie.original_title}}</p>
                     <p v-else>Titolo Originale: {{movie.original_name}}</p>
-                    <p>Lingua Originale: {{movie.original_language}}</p>
+                    <div>Lingua Originale: <img v-if="movie.original_language === 'en'" src="@/assets/img/en.png" alt="EN"> <img v-if="movie.original_language === 'it'" src="@/assets/img/it.png" alt="it"> <span v-if="movie.original_language != 'en' && movie.original_language != 'it'">{{movie.original_language}}</span></div>
                     <p>Voto: {{movie.vote_average}}</p>
                 </li>
             </ul>
